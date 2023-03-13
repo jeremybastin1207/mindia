9ff6cddefab4e75b8f965ccee9d9d571131b5fb3
Jérémy Bastin <jeremybastin1207@gmail.com>
2023-03-13 22:01:31 +0100

    feat: improve automations

--- automation/automation.go
+++ automation/automation.go
@@ -75,34 +75,3 @@ func (a *Automation) Run(actx AutomationCtx, namer namer.Namer, source *Source, 
 
 	return outputs, nil
 }
-
-/* func (a *Automation) DryRun(actx AutomationCtx, namer namer.Namer) (context.Context, error) {
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, AutomationCtxKey{}, actx)
-
-	steps := a.Steps
-	steps = append([]AutomationDoer{
-		NewNamer(&NamerConfig{
-			AutomationStepConfig: &AutomationStepConfig{
-				Children: []*Automation{},
-			},
-			Namer: namer,
-		}),
-	}, steps...)
-
-	var err error
-	for _, step := range steps {
-		ctx, err = step.Do(ctx)
-		if err != nil {
-			return nil, err
-		}
-		for _, child := range step.GetChildren() {
-			actx := ctx.Value(AutomationCtxKey{}).(AutomationCtx)
-			ctx, err = child.DryRun(actx, child.Namer)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	return ctx, nil
-} */
